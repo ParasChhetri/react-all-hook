38c30f990d19b72ba2544852ff314c12e55f9144
ParasChhetri <paraschhetri477@gmail.com>
2025-04-18 22:15:29 +0530

    done useRef hook and useLayout hook begins

--- useref-hook-exp/src/components/Ref.js
+++ useref-hook-exp/src/components/Ref.js
@@ -1,8 +1,25 @@
+import {useEffect, useRef} from 'react';
 const Ref = () => {
+    const countValue = useRef(0);
+    const divRef = useRef();
+    const inputRef = useRef();
+    const handleClick = () => {
+        countValue.current ++
+        // console.log(countValue.current);
+    }
+    useEffect(() => {
+        const getDivRef = divRef.current;
+        console.log(getDivRef);
+    },[]);
+    useEffect(() => {
+        inputRef.current.focus();
+    },[])
     return(
         <>
             <h1>Click Counter with useRef hook</h1>
-
+            <button onClick={handleClick}>click</button>
+            <div ref={divRef} className='helo'>Hello</div>
+            <input ref={inputRef} type='text'/>
         </>
     );
 }
